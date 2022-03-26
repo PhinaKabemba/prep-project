93128bb5e76ac14af98963dc98a8e20e6ede636c
PhinaKabemba <kabembaphina@gmail.com>
2022-03-26 22:46:25 +0200

    Revamped the project per start.spring.io

---
+++ src/main/java/com/prepproject/PrepProjectApplication.java
@@ -0,0 +1,13 @@
+package com.prepproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prepproject/PrepProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prepproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
